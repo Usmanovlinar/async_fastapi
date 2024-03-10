7265c439538b1e6ee3115c0e9e3837f77413d36e
l.usmanov <l.usmanov@agora.ru>
2024-03-11 00:58:41 +0300

    + переменные окружения

--- src/database.py
+++ src/database.py
@@ -2,6 +2,16 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+"""import os
+from dotenv import load_dotenv
+load_dotenv()
+
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_NAME = os.getenv("DB_NAME")
+DB_HOST = os.getenv("DB_HOST")
+DB_PASS = os.getenv("DB_PASS")"""
+
 
 SQLALCHEMY_DATABASE_URL = 'postgresql+asyncpg://postgres:postgres@localhost/postgres'
 
